9f99d3cd2fc68fe8820769f888a8d8c2da2ea947
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-06 16:17:25 +0300

    feat(users): add required email field for user model.

--- blogger/users/models.py
+++ blogger/users/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     """
     User entity.
     """
-    pass
+    email = models.EmailField()
