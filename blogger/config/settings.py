6e070665d167ed5841acef40929de4683fa8fadc
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-07 09:46:44 +0300

    add default pagination class.

--- blogger/config/settings.py
+++ blogger/config/settings.py
@@ -104,5 +104,6 @@ CSRF_TRUSTED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
